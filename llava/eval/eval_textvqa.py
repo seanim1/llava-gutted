75244ad91a2e455ad094aa5775dc41292f9429e3
Sean Im <92773124+seanim1@users.noreply.github.com>
2024-05-02 21:15:20 -0400

    Update eval_textvqa.py

--- llava/eval/eval_textvqa.py
+++ llava/eval/eval_textvqa.py
@@ -14,23 +14,22 @@ def get_args():
     return parser.parse_args()
 
 
-def prompt_processor(prompt):
-    if prompt.startswith('OCR tokens: '):
-        pattern = r"Question: (.*?) Short answer:"
-        match = re.search(pattern, prompt, re.DOTALL)
-        question = match.group(1)
-    elif 'Reference OCR token: ' in prompt and len(prompt.split('\n')) == 3:
-        if prompt.startswith('Reference OCR token:'):
-            question = prompt.split('\n')[1]
-        else:
-            question = prompt.split('\n')[0]
-    elif len(prompt.split('\n')) == 2:
-        question = prompt.split('\n')[0]
-    else:
-        assert False
-
-    return question.lower()
-
+#def prompt_processor(prompt):
+#    if prompt.startswith('OCR tokens: '):
+#        pattern = r"Question: (.*?) Short answer:"
+#        match = re.search(pattern, prompt, re.DOTALL)
+#        question = match.group(1)
+#    elif 'Reference OCR token: ' in prompt and len(prompt.split('\n')) == 3:
+#        if prompt.startswith('Reference OCR token:'):
+#            question = prompt.split('\n')[1]
+#        else:
+#            question = prompt.split('\n')[0]
+#    elif len(prompt.split('\n')) == 2:
+#        question = prompt.split('\n')[0]
+#    else:
+#        assert False
+#
+#    return question.lower()
 
 def eval_single(annotation_file, result_file):
     experiment_name = os.path.splitext(os.path.basename(result_file))[0]
@@ -40,12 +39,31 @@ def eval_single(annotation_file, result_file):
     results = [json.loads(line) for line in open(result_file)]
 
     pred_list = []
+    index = 0
+    annotation_list = list(annotations.items())
     for result in results:
-        annotation = annotations[(result['question_id'], prompt_processor(result['prompt']))]
+    # method one (works universally, either out-of-order or in-order)
+        #question = result['prompt'] # result is a Python Dictionary variable. "prompt should have the question in it"
+        #question_lower = question.lower()  # Convert the question to lowercase, although it is already lowercase.
+        # search through the annotations, and if we find a corresponding question, retrieve the annotation.
+        #matching_annotation = 0
+        #for key, annotation in annotations.items():
+        #    if question == key[1]:  # Check if the question matches
+        #        matching_annotation = annotation
+        #        break
+        #if matching_annotation == 0:
+        #    assert False # question was not found in the annotations
+        #pred_list.append({
+        #    "pred_answer": result['text'],
+        #    "gt_answers": matching_annotation['answers'],
+        #})
+    # method two (only works if the result files are in-order, but faster)
+        matching_annotation = annotation_list[index][1]
         pred_list.append({
             "pred_answer": result['text'],
-            "gt_answers": annotation['answers'],
+            "gt_answers": matching_annotation['answers'],
         })
+        index += 1
 
     evaluator = TextVQAAccuracyEvaluator()
     print('Samples: {}\nAccuracy: {:.2f}%\n'.format(len(pred_list), 100. * evaluator.eval_pred_list(pred_list)))
